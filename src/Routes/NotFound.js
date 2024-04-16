135053b21b4990426289586f107d87f2e103d3ec
NSZer0 <bradatima@gmail.com>
2024-04-16 16:33:54 -0500

    Added a Breadcrumb to the Not Found page

--- src/Routes/NotFound.js
+++ src/Routes/NotFound.js
@@ -1,9 +1,11 @@
 import React from "react";
+import PathDisplay from "../Components/PathDisplay";
 
 function NotFound() {
   return (
     <div className="NotFound">
-      <h1>Not Found</h1>
+      <PathDisplay activePathName="Page Not Found"/>
+      <h1>Page Not Found</h1>
     </div>
   );
 }
